146f388ecc55a439103bee62b544f8e81971bc38
buddhika deshapriya <buddhika384@gmail.com>
2021-02-22 08:41:15 +0530

    branch code page url modified

--- src/main/java/com/example/easyloan/management/Controller/BranchCodeGenController.java
+++ src/main/java/com/example/easyloan/management/Controller/BranchCodeGenController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/loanapplicationinvoicenumbergenerate")
+@RequestMapping("/branchcodegenerate")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class BranchCodeGenController {
     private BranchCodeGenService branchCodeGenService;
